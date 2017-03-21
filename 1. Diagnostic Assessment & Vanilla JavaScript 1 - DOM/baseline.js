b47cd60214d050c7d6ecf662b9db9b30d01bee33
twof <fabiobean2@gmail.com>
2017-03-21 14:30:52 -0700

    Did baseline challenges

--- 1. Diagnostic Assessment & Vanilla JavaScript 1 - DOM/baseline.js
+++ 1. Diagnostic Assessment & Vanilla JavaScript 1 - DOM/baseline.js
@@ -1,19 +1,23 @@
 // `$('#todos')`
-
+document.getElementById("todos");
 
 // `.text()`
-
+element.textContent;
 
 // `.text("hello!")`
-
+element.innerHTML = "hello!";
 
 // `.val()`
+element.children[0];
 
-
-// `.prepend()`
+// `.prepend("hello")`
+document.getElementById("example").innerHTML = "hello" + document.getElementById("example").innerHTML;
 
 
 // `.addClass('bold')`
-
+element.classList.add('bold');
 
 // $(document).ready()
+document.addEventListener("DOMContentLoaded", function(event) {
+    
+});
